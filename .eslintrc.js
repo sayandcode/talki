b327abdf8d1288e4b2075b29e80e5ed5a23f0088
sayandcode <sayandcode@gmail.com>
2023-01-06 18:06:35 +0530

    Fix typescript and eslint alias errors
    Typescript alias issues were fixed using tsconfig-paths for ts-node, and
    tsc-alias package for build time.
    
    Eslint module resolution errors were fixed using the
    eslint-config-airbnb-typescript package

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,16 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: "./tsconfig.eslint.json",
   },
   plugins: ["@typescript-eslint", "prettier"],
   rules: {},
