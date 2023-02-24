f85dd86da393835ea823e836f2288cac7868f102
sayandcode <sayandcode@gmail.com>
2023-02-24 12:21:19 +0530

    Add SSL support for localhost development
    Using vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import CSPConfigStr from "./configHelpers/csp";
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+  },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
