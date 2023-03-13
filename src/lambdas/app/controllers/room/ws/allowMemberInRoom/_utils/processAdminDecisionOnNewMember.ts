8d4c3f041e7d496619d3aac8d7c4867cc293d5be
sayandcode <sayandcode@gmail.com>
2023-03-13 11:32:05 +0530

    Make askOtherMembersToConnectToNewMember common to all lambdas
    This way, the authorizer route can also run the logic of asking other
    members to connect to the new member.

--- src/lambdas/app/controllers/room/ws/allowMemberInRoom/_utils/processAdminDecisionOnNewMember.ts
+++ src/lambdas/app/controllers/room/ws/allowMemberInRoom/_utils/processAdminDecisionOnNewMember.ts
@@ -1,6 +1,7 @@
 import wsBackendForApp from "@appLambda/utils/wsBackend";
 import { RoomDocument } from "models/Room/index.model";
 import { ConnectedRoomMember } from "models/Room/schemas/member/index.schema";
+import askOtherMembersToConnectToNewMember from "@utils/askOtherMembersToConnectToNewMember";
 
 async function processAdminDecisionOnNewMember({
   isNewMemberAllowedInRoomByAdmin,
@@ -17,6 +18,11 @@ async function processAdminDecisionOnNewMember({
   } else {
     // eslint-disable-next-line no-param-reassign
     newMember.isAllowedInRoom = true;
+    await askOtherMembersToConnectToNewMember(
+      requestedRoom,
+      newMember.memberId,
+      wsBackendForApp
+    );
   }
   await requestedRoom.save();
 }
